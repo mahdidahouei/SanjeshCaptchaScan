a379c0c91f28ebd0fb3617418e84c46c7f6543c0
Mahdi <mahdi.a.200066@gmail.com>
2020-04-07 04:40:08 +0430

    Intialize commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SanjeshCaptchaScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.exitButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(856, 12);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(32, 31);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Location = new System.Drawing.Point(818, 12);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(32, 31);
+            this.minimizeButton.TabIndex = 2;
+            this.minimizeButton.Text = "-";
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            this.minimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(547, 98);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(226, 145);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(461, 49);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(427, 235);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 2);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 451);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(900, 2);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2, 449);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(898, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(2, 449);
+            this.panel4.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox1.Location = new System.Drawing.Point(645, 351);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 91);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Image = ((System.Drawing.Image)(resources.GetObject("refreshButton.Image")));
+            this.refreshButton.Location = new System.Drawing.Point(12, 12);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(32, 31);
+            this.refreshButton.TabIndex = 13;
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox2.Location = new System.Drawing.Point(196, 351);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 91);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(6, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 31);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Scaned from original image:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(455, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 31);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Scaned from final image:";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel5.Location = new System.Drawing.Point(450, 2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1, 449);
+            this.panel5.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 453);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.exitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Tesseract;
+
+namespace SanjeshCaptchaScan
+{
+
+    public partial class Form1 : Form
+    {
+        private Point MouseLocation;
+        private Point MouseLocation1;
+        private Point MouseLocation2;
+        private string siteUrl = "http://92.242.195.165/ci_sherkat/index.php/karname_nah/m13";
+        private string imageURL;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { webBrowser1.Navigate(siteUrl); } //open the site
+
+        private async void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Bitmap image;
+            try
+            {
+                this.imageURL = webBrowser1.Document.GetElementsByTagName("img")[0].GetAttribute("src");
+                Task<Bitmap> task = new Task<Bitmap>(DownloadImage);
+                task.Start();
+                image = await task; //download image in other thread
+                if (image == null) throw new Exception("image is null"); //if there was a problem downloading the image, throw an exception
+            }
+            catch
+            {
+                MessageBox.Show("Connection error");
+                return; 
+            }
+
+            Bitmap middleImage = new Bitmap(image, image.Width, image.Height); //make a new bitmap 
+
+            int R = 105, G = 126, B = 177; // this color is a little bit darker than the background of our image.. so we can work with the pixels that are in background or not!
+
+            int left = middleImage.Width, top = middleImage.Height, right = 0, bottom = 0; // we're ganna calculate the height and width of the text in the picture so we need to find top, bottom, left, and right pixel of the text
+
+            /*
+             * loop through all of the pixels in bitmap
+             */
+            for (int i = 0; i < image.Width; i++)
+            {
+                for(int j = 0; j < image.Height; j++)
+                {
+                    if(Convert.ToInt64(image.GetPixel(i, j).ToArgb()) < Convert.ToInt64(Color.FromArgb(R, G, B).ToArgb())) // if this condition is true the pixel is in text
+                    { // findind left, top, right, and bottom of the text
+                        if (i < left) left = i - 1;
+                        if (j <= top) top = j - 1;
+                        if (i >= right) right = i + 1;
+                        if (j > bottom) bottom = j + 1;
+                    }
+                }
+            }
+
+            Bitmap finalImage = new Bitmap(middleImage.Width, middleImage.Height); //make a new bitmap for pictureBox2
+
+            int textWidth = right - left, textHeight = bottom - top; // now we calculate the width and height of the text
+
+            int finalLeft = (middleImage.Width - textWidth) / 2, finalTop = (middleImage.Height - textHeight) / 2; // if the distance of the text from the top and left becomes like finalTop and finalLeft so the text will be in the center of the image
+
+            int leftDifference = finalLeft - left, topDifference = finalTop - top; // we need to add topDifference and LeftDifference to current top and left, so the text will be in the center of image
+
+            int maxLeftSquare = 0, bottomLeftX = 0, bottomLeftY = 0, maxRightSquare = 0, bottomRightX = 0, bottomRightY = 0; // we're ganna find two pixels! one is the most left and bottommost pixel of the text, the other is the most right and bottommost pixel of the text
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    if (Convert.ToInt64(image.GetPixel(i, j).ToArgb()) < Convert.ToInt64(Color.FromArgb(R, G, B).ToArgb()))
+                    {
+                        int x = i + leftDifference;
+                        int y = j + topDifference;
+
+                        finalImage.SetPixel(x, y, middleImage.GetPixel(i, j));// draw the text in the center of finalImage
+
+                        int leftArea = i * j, rightArea = (finalImage.Width - i) * j; 
+
+                        if (leftArea > maxLeftSquare)
+                        { // finding bottomLeft pixel
+                            maxLeftSquare = leftArea;
+                            bottomLeftX = x;
+                            bottomLeftY = y;
+                        }
+                        if (rightArea > maxRightSquare)
+                        { // finding bottomRight pixel
+                            maxRightSquare = rightArea;
+                            bottomRightX = x;
+                            bottomRightY = y;
+                        }
+                    }
+                }
+            }
+
+            //finalImage.SetPixel(bottomLeftX, bottomLeftY, Color.Red); //bottom left pixel of captcha
+            //finalImage.SetPixel(bottomRightX, bottomRightY, Color.Red); //bottom right pixel of captcha
+
+
+            double gradient = (double) (bottomLeftY - bottomRightY) / (bottomRightX - bottomLeftX); // gradient of the text
+            double radian = Math.Atan(gradient); // this in the angle that we need to ratate the text, so it'll become horizontal
+            short degree = Convert.ToInt16(radian * (180.0 / Math.PI)); // convert the angle to degree
+
+            finalImage = rotateImage(finalImage, degree);
+
+            // scanning original image
+            TesseractEngine engineImage = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+            Page imagePage = engineImage.Process(image, PageSegMode.Auto);
+
+            // scanning finalImage
+            TesseractEngine engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+            Page page = engine.Process(finalImage, PageSegMode.Auto);
+            string imageNumber = page.GetText();
+
+            if (correctScanedNumber(imageNumber).Equals("false")) webBrowser1.Navigate(siteUrl);
+            else // show information to user
+            {
+                pictureBox1.Image = image;
+                pictureBox2.Image = finalImage;
+                textBox1.Text = correctScanedNumber(imageNumber); // check some special characters
+                textBox2.Text = imagePage.GetText();
+            }
+        }
+
+        private Bitmap DownloadImage()
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(imageURL);
+                WebResponse response = request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                return new Bitmap(responseStream);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private Bitmap rotateImage(Bitmap bitmap, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(bitmap.Width, bitmap.Height);
+            Graphics graphics = Graphics.FromImage(rotatedImage);
+            Rectangle ImageSize = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            graphics.FillRectangle(Brushes.White, ImageSize); // brush background to white
+            graphics.TranslateTransform((float)bitmap.Width / 2, (float)bitmap.Height / 2);
+            graphics.RotateTransform(angle);
+            graphics.TranslateTransform(-(float)bitmap.Width / 2, -(float)bitmap.Height / 2);
+            graphics.DrawImage(bitmap, new Point(0, 0)); // draw rotated image
+            return rotatedImage;
+        }
+
+        private string correctScanedNumber(string imageNumber)
+        {
+            imageNumber = imageNumber.ToLower();
+            string number = "";
+            for(int i = 0; i < imageNumber.Length; i++)
+            {
+                if (isLegal(imageNumber[i]))
+                {
+                    if (imageNumber[i] == 'o' || imageNumber[i] == 'd' || imageNumber[i] == 'b' || imageNumber[i] == 'a' || imageNumber[i] == '9') number += "0";
+                    else if (imageNumber[i] == '?') number += "2";
+                    else if (imageNumber[i] == '$' || imageNumber[i] == '%') number += "35";
+                    else if (imageNumber[i] == '8') number += "3";
+                    else if (imageNumber[i] == '6' || (imageNumber[i] == 'f' && imageNumber[i + 1] == ')')) number += "5";
+                    else if (imageNumber[i] == '\"') number += "41";
+                    else if (imageNumber[i] == ':' && imageNumber[i + 1] == 'i')
+                    {
+                        number += "3";
+                        i++;
+                    }
+                    else if ((imageNumber[i] == '|' && imageNumber[i + 1] == ']') || (imageNumber[i] == 'i' && imageNumber[i + 1] == ']') ||
+                             (imageNumber[i] == '!' && imageNumber[i + 1] == ']') || (imageNumber[i] == '|' && imageNumber[i + 1] == '}') ||
+                             (imageNumber[i] == 'i' && imageNumber[i + 1] == '}') || (imageNumber[i] == '!' && imageNumber[i + 1] == '}'))
+                    {
+                        number += "0";
+                        i++;
+                    }
+                    else if (imageNumber[i] == 'i' || imageNumber[i] == '!' || imageNumber[i] == 'f' || imageNumber[i] == '7') number += "1";
+                    else number += imageNumber[i].ToString();
+                }
+            }
+            if (number.Length == 5) return number;
+            else return "false";
+        }
+
+        private bool isLegal(char number)
+        {
+            switch (number)
+            {
+                case ')':
+                case '%':
+                case '}':
+                case ']':
+                case '|':
+                case '\"':
+                case 'a':
+                case 'f':
+                case 'b':
+                case 'd':
+                case 'i':
+                case '?':
+                case '!':
+                case '$':
+                case '0':
+                case '9':
+                case '8':
+                case '7':
+                case '6':
+                case '5':
+                case '4':
+                case '3':
+                case '2':
+                case '1':
+                    return true;
+                default: return false;
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(siteUrl);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePosition = Control.MousePosition;
+                mousePosition.Offset(MouseLocation.X, MouseLocation.Y);
+                Location = mousePosition;
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseLocation1 = new Point(-e.X, -e.Y);
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePosition = Control.MousePosition;
+                mousePosition.Offset(MouseLocation1.X - pictureBox1.Left, MouseLocation1.Y - pictureBox1.Top);
+                Location = mousePosition;
+            }
+        }
+
+        private void PictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseLocation2 = new Point(-e.X, -e.Y);
+        }
+
+        private void PictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePosition = Control.MousePosition;
+                mousePosition.Offset(MouseLocation2.X - pictureBox2.Left, MouseLocation2.Y - pictureBox2.Top);
+                Location = mousePosition;
+            }
+        }
+    }
+}
